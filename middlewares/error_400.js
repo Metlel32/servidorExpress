ee948a7df5591ec408713743bea59fd2097a31e2
Luis Macias <metlel32@gmail.com>
2025-03-18 09:12:47 -0500

    proyecto finalizado

--- middlewares/error_400.js
+++ middlewares/error_400.js
@@ -3,7 +3,7 @@ const error_400 = (error, req, res, next) => {
     if (error.message == "bad request") {
         return res.status(400).json({
             succes: false,
-            message: "error",
+            message: "Invalid request. Please check your input and try again",
             error: error
         })
     }
--- server.js
+++ server.js
@@ -7,19 +7,24 @@ import routerIndex from "./routes/index.js"
 import  not_found_handler from "./middlewares/not_found_handler.js"
 import error_handler from "./middlewares/error_handler.js"
 import error_400 from "./middlewares/error_400.js"
+import blocked_ip from "./middlewares/blocked_ip.js"
 
 const server = express()
 const PORT = process.env.PORT || 8080
 const ready = ()=> console.log("server ready in port: " + PORT);
+
 
 server.use(express.urlencoded({extended:true}))
 server.use(express.json())
 server.use(cors())
 server.use(morgan('dev'))
 
+
+
+server.use(blocked_ip)
 server.use('/api', routerIndex)
 server.use(not_found_handler)
 server.use(error_400)
 server.use(error_handler)
 
-server.listen(PORT, ready)
+server.listen(PORT, "0.0.0.0", ready)
